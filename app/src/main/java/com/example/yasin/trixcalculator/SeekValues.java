51d2f57dbdc093b83c5a5f4d1633edbcfa6e1ecc
omrysn4 <omrysn4@gmail.com>
2017-12-19 13:25:06 -0700

    initial commit

---
+++ app/src/main/java/com/example/yasin/trixcalculator/SeekValues.java
@@ -0,0 +1,32 @@
+package com.example.yasin.trixcalculator;
+
+import android.widget.SeekBar;
+
+/**
+ * Created by yasin on 08/12/17.
+ */
+
+public class SeekValues {
+
+    public static int SeekValues(SeekBar seek) {
+
+        int progress = seek.getProgress();
+
+        int max = seek.getMax();
+
+        int mid = max/2;
+
+        int diff = progress - mid;
+
+        if(diff > 0){
+
+            return  max-progress;
+        }else if(diff < 0){
+            return -diff-1;
+        }else{
+
+            return mid;
+        }
+
+    }
+}
